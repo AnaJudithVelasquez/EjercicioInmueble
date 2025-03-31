39ceab89801855bad1a996636eafd68b8654c7c4
Ana Velasquez <anajudith1309@gmail.com>
2025-03-30 20:22:53 -0500

    Maria ya añadí los atributos y metodos de la clase Inmueble.

--- EjercicioInmuebleVP/src/ejercicioinmueblevp/Inmueble.java
+++ EjercicioInmuebleVP/src/ejercicioinmueblevp/Inmueble.java
@@ -6,5 +6,27 @@ package ejercicioinmueblevp;
 
 
 public class Inmueble {
-    
+    int identificadorInmobiliario;
+     int area;
+     String direccion;
+     double precioVenta;
+
+    public Inmueble(int identificadorInmobiliario, int area, String direccion, double precioVenta) {
+        this.identificadorInmobiliario = identificadorInmobiliario;
+        this.area = area;
+        this.direccion = direccion;
+        this.precioVenta = precioVenta;
+    }
+     
+     public double calcularPrecioVenta(double valorArea){
+         
+         this.precioVenta = area * valorArea;
+        return precioVenta;   
+ }
+     public void imprimir(){
+         System.out.println(" Identificador: " + identificadorInmobiliario);
+         System.out.println(" Area: " + area + " m^2 ");
+         System.out.println(" Direccion:" + direccion);
+         System.out.println(" Precio de Venta: $ " + precioVenta);
+     }
 }
